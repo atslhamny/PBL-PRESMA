0e9c2a49172e4fc203c1185ba0d303c31a0cef76
Atsilah Amany <atsilahamanyph@gmail.com>
2024-12-09 22:09:26 +0700

    modify: index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!-- index -->
- 
 <?php
 include('lib/Session.php');
 $session = new Session();
@@ -19,16 +17,16 @@ if ($session->get('is_login') !== true) {
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap4.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
   <!-- jQuery -->
-  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -90,26 +88,25 @@ if ($session->get('is_login') !== true) {
   </div>
 
   <!-- Bootstrap 4 -->
-  <script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
   <!-- jQuery Validation -->
-  <script src="adminlte/plugins/jquery-validation/jquery.validate.min.js"></script>
-  <script src="adminlte/plugins/jquery-validation/additional-methods.min.js"></script>
-  <script src="adminlte/plugins/jquery-validation/localization/messages_id.min.js"></script>
-  <!-- DataTables & Plugins -->
-  <script src="adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-  <script src="adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-  <script src="adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-  <script src="adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-  <script src="adminlte/plugins/jszip/jszip.min.js"></script>
-  <script src="adminlte/plugins/pdfmake/pdfmake.min.js"></script>
-  <script src="adminlte/plugins/pdfmake/vfs_fonts.js"></script>
-  <script src="adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-  <script src="adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-  <script src="adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"></script>
+  <!-- DataTables -->
+  <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.bootstrap4.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="adminlte/dist/js/adminlte.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
